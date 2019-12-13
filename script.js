bf02b724675664783d96cdb51dcdfc1d3aa17f4c
mictmurp <mictmurp@iu.edu>
2019-12-13 00:11:00 +0000

    Font sizes/various formatting settings

--- script.js
+++ script.js
@@ -2,16 +2,17 @@
 var myData = [
   {
     front: "PlumberWrench.png",
-    /*credit: "Photo by Wikipedia",
-    filename: "PlumberWrench.png"*/
+    //credit: "Photo by Wikipedia",
     back: "<b>Plumber Wrench</b></br>Handtool</br>A plumber wrench is a pipe wrench used to rotate plumbing pipes. It is adjusted to different pipe diameters by rotating the key ring. Its advantage is that it grips with significant force without needing to engage a nut."
   },
   {
     front: "CompoundMiterSaw.png",
+    //credit: "Photo by DeWalt"
     back: "<b>Compound Miter Saw</b></br>Power Tool</br>A specialized tool that lets you make cuts at a variety of angles featuring blades that pivot left and right for angled cuts and tilt in a single direction for beveled cuts. Manipulating the saw on both of its axes at once yields a compound miter cut."
   },
   {
     front: "HammerDrill.png",
+    //credit: "Photo by DeWalt"
     back: "<b>Hammer Drill</b></br>Power Tool</br>A power drill that works by delivering a rapid succession of blows, used chiefly for drilling in masonry or rock."
   }
 ];
